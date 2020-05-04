43815eda3bf843518e1f9069b3b2a2dfc6c29d55
Christopher Leggett <chris@leggett.dev>
2020-05-03 22:10:03 -0400

    Start of paging implementation.
    Signed-off-by: Christopher Leggett <chris@leggett.dev>

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,7 +1,8 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use crate::println;
 use crate::print;
 use crate::gdt;
+use crate::hlt_loop;
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
 use spin;
@@ -37,6 +38,8 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
 
@@ -106,6 +109,18 @@ extern "x86-interrupt" fn double_fault_handler(
     stack_frame: &mut InterruptStackFrame, _error_code: u64
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+    println!("EXCEPTION: Page Fault");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 /* TESTS */
--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,15 @@ pub extern "C" fn _start() -> ! {
   // trigger a stack overflow
   // stack_overflow();
 
+  // trigger a page fault
+  // let ptr = 0xdeadbeaf as *mut u32;
+  // unsafe { *ptr = 42; }
+
+  use x86_64::registers::control::Cr3;
+
+  let (level_4_page_table, _) = Cr3::read();
+  println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
   #[cfg(test)]
   test_main();
 
