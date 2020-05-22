4444b5c95b4c41824d6a871fdae4631a0f3d5c16
Christopher Leggett <chris@leggett.dev>
2020-05-22 00:21:32 -0400

    Implements proper frame allocator.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
   use blog_os::memory;
-  use x86_64::{structures::paging::MapperAllSizes, VirtAddr};
+  use blog_os::memory::BootInfoFrameAllocator;
+  use x86_64::{structures::paging::Page, VirtAddr};
 
   println!("Hello World{}", "!");
 
@@ -22,25 +23,10 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
   blog_os::init();
 
   let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-
-  let mapper = unsafe { memory::init(phys_mem_offset) };
-
-  let addresses = [
-    // the identity-mapped vga buffer page
-    0xb8000,
-    // some code page
-    0x201008,
-    // some stack page
-    0x0100_0020_1a10,
-    // virtual address mapped to physical address 0
-    boot_info.physical_memory_offset,
-  ];
-
-  for &address in &addresses {
-    let virt = VirtAddr::new(address);
-    let phys = mapper.translate_addr(virt);
-    println!("{:?} -> {:?}", virt, phys);
-  }
+  let mut mapper = unsafe { memory::init(phys_mem_offset) };
+  let mut frame_allocator = unsafe {
+    BootInfoFrameAllocator::init(&boot_info.memory_map)
+  };
 
   #[cfg(test)]
   test_main();
